ab92ca0a5b209fa5c47d03719066bb574de303fa
Dane Petersen <thegreatsunra@gmail.com>
2018-02-13 13:30:22 -0600

    Set a default address if an argument isn't provided
    Rather than erroring out

--- index.js
+++ index.js
@@ -18,12 +18,7 @@
 const { TransactionProcessor } = require('sawtooth-sdk/processor')
 const IntegerKeyHandler = require('./intkey_handler')
 
-if (process.argv.length < 3) {
-  console.log('missing a validator address')
-  process.exit(1)
-}
-
-const address = process.argv[2]
+const address = process.argv[2] || 'tcp://localhost:4004'
 
 const transactionProcessor = new TransactionProcessor(address)
 

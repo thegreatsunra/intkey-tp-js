fe1e285ab2ec362ce1ea14329651c17cf4e39694
Dane Petersen <thegreatsunra@gmail.com>
2018-02-13 13:01:58 -0600

    Add code from intkey_javascript sawtooth-core example
    Source: https://github.com/hyperledger/sawtooth-core/tree/master/sdk/examples/intkey_javascript

--- index.js
+++ index.js
@@ -15,3 +15,20 @@
  * ------------------------------------------------------------------------------
  */
 
+const { TransactionProcessor } = require('sawtooth-sdk/processor')
+const IntegerKeyHandler = require('./intkey_handler')
+
+if (process.argv.length < 3) {
+  console.log('missing a validator address')
+  process.exit(1)
+}
+
+const address = process.argv[2]
+
+const transactionProcessor = new TransactionProcessor(address)
+
+transactionProcessor.addHandler(new IntegerKeyHandler())
+
+transactionProcessor.start()
+
+console.log('started transaction processor')
